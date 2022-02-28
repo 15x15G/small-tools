3bc652d90fd7c5b7c0d0a6238bffd68f715eb44f
15x15G <65473527+15x15G@users.noreply.github.com>
2022-02-28 14:43:46 +0800

    colorful

--- bat/7z/7zpw.bat
+++ bat/7z/7zpw.bat
@@ -1,9 +1,9 @@
 @echo off 
 chcp 65001
 SETLOCAL EnableDelayedExpansion
-
+cls
 for %%i in (%*) do (
-    echo %%i
+    echo [1m%%i[0m
     call :inner %%i  
 )
 pause
@@ -11,15 +11,16 @@ goto :eof
 
 
 :inner
+    SET /A n = 0
     for /F "usebackq tokens=*" %%A in ("password.txt") do (
         "C:\Program Files\7-Zip\7z.exe" t %1 -sccUTF-8 "-p%%A" >nul 2>nul && (
-        echo [OK] %%A 
-        start /b "" "C:\Program Files\7-Zip\7z.exe" x  %1 -aou -sccUTF-8 "-p%%A" -o*~
+        echo [42m [OK] %%A [0m
+        start /b "" "C:\Program Files\7-Zip\7z.exe" x  %1 -aou -sccUTF-8 "-p%%A" -o*~  >nul
         goto :break
         ) || (
-        echo [error] %%A 
+        SET /a n+=1
         )
     )
-    echo [password no find]
+    echo [41m [password no find. tried %n% times.] [0m
     :break
     goto :eof
