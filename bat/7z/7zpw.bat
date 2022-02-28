0498bb08e9dcfba36dda4c2a2d7ebfe93ae42790
15x15G <65473527+15x15G@users.noreply.github.com>
2022-02-28 16:08:28 +0800

    7zpw fix path

--- bat/7z/7zpw.bat
+++ bat/7z/7zpw.bat
@@ -1,7 +1,9 @@
 @echo off 
-chcp 65001
 SETLOCAL EnableDelayedExpansion
+chcp 65001
 cls
+set "exe=C:\Program Files\7-Zip\7z.exe"
+set "pwlist=D:\新建文件夹\Github\small-tools\bat\7z\password.txt"
 for %%i in (%*) do (
     echo [1m%%i[0m
     call :inner %%i  
@@ -12,10 +14,11 @@ goto :eof
 
 :inner
     SET /A n = 0
-    for /F "usebackq tokens=*" %%A in ("password.txt") do (
-        "C:\Program Files\7-Zip\7z.exe" t %1 -sccUTF-8 "-p%%A" >nul 2>nul && (
+    cd /d %~dp1
+    for /F "usebackq tokens=*" %%A in ("%pwlist%") do (
+        "%exe%" t %1 -sccUTF-8 "-p%%A" >nul 2>nul && (
         echo [42m [OK] %%A [0m
-        start /b "" "C:\Program Files\7-Zip\7z.exe" x  %1 -aou -sccUTF-8 "-p%%A" -o*~  >nul
+        "%exe%" x  %1 -aou -sccUTF-8 "-p%%A" -o*~  >nul
         goto :break
         ) || (
         SET /a n+=1
