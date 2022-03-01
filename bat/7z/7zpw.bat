a31339fbe6c7ccc5f50a806e54bceefd5472d827
15x15g <65473527+15x15G@users.noreply.github.com>
2022-03-01 21:02:07 +0800

    use 7zg

--- bat/7z/7zpw.bat
+++ bat/7z/7zpw.bat
@@ -2,13 +2,12 @@
 SETLOCAL EnableDelayedExpansion
 chcp 65001
 cls
-set "exe=C:\Program Files\7-Zip\7z.exe"
-set "pwlist=D:\新建文件夹\Github\small-tools\bat\7z\password.txt"
+set "exe=C:\Program Files\7-Zip\7z"
+set "pwlist=password.txt"
 for %%i in (%*) do (
     echo [1m%%i[0m
     call :inner %%i  
 )
-pause
 goto :eof
 
 
@@ -16,9 +15,9 @@ goto :eof
     SET /A n = 0
     cd /d %~dp1
     for /F "usebackq tokens=*" %%A in ("%pwlist%") do (
-        "%exe%" t %1 -sccUTF-8 "-p%%A" >nul 2>nul && (
+        "%exe%" l %1 -sccUTF-8 "-p%%A" >nul 2>nul && (
         echo [42m [OK] %%A [0m
-        "%exe%" x  %1 -aou -sccUTF-8 "-p%%A" -o*~  >nul
+        "%exe%g" x  %1 -aou -sccUTF-8 "-p%%A" -o*~  
         goto :break
         ) || (
         SET /a n+=1
