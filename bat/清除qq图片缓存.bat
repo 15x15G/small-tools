803d9dce9a732da9a4059e1efebc0cdc44df5b95
15x15g <65473527+15x15G@users.noreply.github.com>
2022-03-09 00:07:33 +0800

    fixbug

--- bat/清除qq图片缓存.bat
+++ bat/清除qq图片缓存.bat
@@ -1,3 +1,11 @@
-set folder="D:\Documents\Tencent Files\123456789\Image\Group2"
-cd /d %folder%
+@echo off
+set folder="D:\Documents\Tencent Files\123456789\Image\Group2" 
+cd /d %folder% && (
+echo deleting...
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+echo finish
+) || (
+echo path error
+)
+
+pause
