9ea1b8eb6476e612c000a3860c212c847c0d890d
15x15G <65473527+15x15G@users.noreply.github.com>
2022-06-06 15:51:43 +0800

    update hash

--- bat/hash/sha1.bat
+++ bat/hash/sha1.bat
@@ -1,5 +1,10 @@
 @echo off
-
+if "%~1"=="" goto blank
 set a=%~n0
 
 powershell -NoProfile -Command "& {$string = (Get-FileHash $args[1] -Algorithm $args[0]).hash; Add-Type -AssemblyName PresentationFramework;$UserResponse=[System.Windows.MessageBox]::Show(\"$string`n`nClick OK to copy to clipboard\", $args, 1);if($UserResponse -eq \"OK\"){Set-Clipboard -Value $string}}" "%a%" """%1""" 
+exit
+
+:blank
+echo parameter required
+pause
